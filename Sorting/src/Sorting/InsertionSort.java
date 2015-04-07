9206067b6b76814d3a8717b5f8fe4ef223c4cb34
Carlos Rotger <carlos_rotger@brown.edu>
2015-04-07 18:54:59 -0400

    Implemented insertionsort

--- Sorting/src/Sorting/InsertionSort.java
+++ Sorting/src/Sorting/InsertionSort.java
@@ -8,8 +8,25 @@ public class InsertionSort {
 	 * @return					the input array, sorted least to greatest
 	 */
 	public static int[] insertionSort(int[] unsortedArray) {
-		//TODO: S2 implements insertion sort
-		
-		return null;
-	}
+	    // TODO Implement insertion sort
+		  
+		  for(int i=1; i < unsortedArray.length; i++){
+			  int x = unsortedArray[i];
+			  int j = i;
+			  while((j > 0) && unsortedArray[j-1] > x){
+				  unsortedArray[j] = unsortedArray[j-1];
+				  j = j - 1;
+			  }
+			  unsortedArray[j] = x;
+		  }
+
+	    return unsortedArray;
+	  }
+  
+	  private static int[] swap(int array[], int pos1, int pos2){
+		  int temp = array[pos1];
+		  array[pos1] = array[pos2];
+		  array[pos2] = temp;
+		  return array;
+	  }
 }
